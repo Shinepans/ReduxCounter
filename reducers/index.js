5171ee12e0223712c2ae2cfa77cad28bb7f9d8b4
Shinepans <shinepans@live.com>
2016-02-15 13:43:06 +0800

    redux counter to learn it's mode

--- reducers/index.js
+++ reducers/index.js
@@ -1,13 +1,10 @@
 export default function counter(state = 0, action) {
-    switch (action.type) {
-        //增加计数
-        case 'INCREMENT':
-            return state + 1
-        //减少计数
-        case 'DECREMENT':
-            return state - 1
-        //没有操作则直接返回state
-        default:
-            return state
-    }
+  switch (action.type) {
+    case 'INCREMENT':
+      return state + 1
+    case 'DECREMENT':
+      return state - 1
+    default:
+      return state
+  }
 }
